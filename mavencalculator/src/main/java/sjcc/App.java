36a340308cccf7bb3012713f5ee34df91793bcbe
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2023-09-19 15:39:47 -0700

    created and printed menu
    create and print menu for the functionalities of the calculator

--- mavencalculator/src/main/java/sjcc/App.java
+++ mavencalculator/src/main/java/sjcc/App.java
@@ -8,6 +8,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("****************************************\n");
+        System.out.println("    Chavvi Calculator    \n");
+        System.out.println("****************************************\n");
+        System.out.println("    A = 0.0000    |    B = 0.0000\n");
+        System.out.println("****************************************\n");
+        System.out.println("a    :    add value for A\n");
+        System.out.println("b    :    add value for B\n");
+        System.out.println("+    :    addition\n");
+        System.out.println("-    :    subtraction\n");
+        System.out.println("*    :    multiplication\n");
+        System.out.println("/    :    division\n");
+        System.out.println("c    :    clear\n");
+        System.out.println("q    :    quit\n");
+        System.out.println("****************************************\n");
+
+
+
+        
     }
 }
