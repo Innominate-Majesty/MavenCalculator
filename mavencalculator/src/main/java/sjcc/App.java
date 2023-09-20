1f51e945e8f9754b608c753084d96d05901ab787
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2023-09-20 15:33:54 -0700

    added Scanner and Switch cases (calculator operations)
    1. added Scanner to get the user input value and commands for the calculator
    2. added Switch cases to have the different operations that my calculator offers
    3. fix some syntax error issues/formatting of the code
    4. Running to make sure it's actually printing something

--- mavencalculator/src/main/java/sjcc/App.java
+++ mavencalculator/src/main/java/sjcc/App.java
@@ -1,17 +1,60 @@
 package sjcc;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
+
+        Scanner userInput = new Scanner(System.in);
+
         double A = 0.0;
         double B = 0.0;
 
         printMenu(A, B);
+
+        String command;
+        do{
+            System.out.print("Enter a command: ");
+            command = userInput.next().trim();
+
+            switch (command) {
+                case "a":
+                    System.out.print("Enter the value for A: ");
+                    A = userInput.nextDouble();
+                    break;
+                case "b":
+                    System.out.print ("Enter the value for B: ");
+                    B = userInput.nextDouble();
+                    break;
+                case "+":
+                    System.out.println("Result: " + String.format("%.3f", A + B));
+                    break;
+                case "-":
+                    System.out.println("Result: " + String.format("%.3f", A - B));
+                    break;
+                case "*":
+                    System.out.println("Result: " + String.format("%.3f", A * B));
+                    break;
+                case "/":
+                    System.out.println("Result: " + String.format("%.3f", A / B));
+                    break;
+                case "q":
+                    System.out.println("Quitting..........");
+                    break;
+                default:
+                    System.out.println("ERROR"); 
+
+            }
+
+            printMenu(A, B);
+
+        }
+
+        while (!command.equals("q"));
+
+        userInput.close();
     }
 
     public static void printMenu(double A, double B){
